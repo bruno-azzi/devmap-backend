a08a2a6086a9ecf7c9ade8b306381c1ce4155a5d
Bruno Lessa Azzi <bruno.azzi@intranet.rca.net>
2020-02-04 14:34:31 -0300

    heroku port

--- src/index.js
+++ src/index.js
@@ -14,4 +14,8 @@ app.use(cors());
 app.use(express.json()); // tem q vir antes do app.use(routes);
 app.use(routes);
 
-app.listen('/'); 
+// app.listen(3333);
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
